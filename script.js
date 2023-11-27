fdd6f8e690911d7bb49b02ba453e86ce89891779
mobindas m <146184535+mobindaz@users.noreply.github.com>
2023-11-27 16:06:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Web Developer", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+
+       
